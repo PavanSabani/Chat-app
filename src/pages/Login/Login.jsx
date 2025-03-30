2dc87e85bfa196e025be058fb427d0934f0a25e3
PavanSabani <pavansabani12@gmail.com>
2025-03-30 14:24:09 +0530

    chatbox panel created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css';
 import assets from '../../../assets/assets';
 
 
 const Login = () => {
+    const [currentState,setCurrentState] = useState("Sign up");
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="" className='logo' />
         <form className="login-form">
-            <h2>Sign Up</h2>
-            <input type="text" placeholder='Username' className='form-input' required />
-            <input type="email" placeholder='Email' className='form-input' />
-            <input type="password" placeholder='Password' className='form-input' />
-            <button type='submit'>Sign Up</button>
+            <h2>{currentState}</h2>
+            {currentState==="Sign up" ?<input type="text" placeholder='Username' className='form-input' required />:null}
+            <input type="email" placeholder='Email' className='form-input' required />
+            <input type="password" placeholder='Password' className='form-input' required />
+            <button type='submit'>{currentState==="Sign up"?"Create account":"Login"}</button>
             <div className="login-term">
                 <input type="checkbox" />
                 <p>Agree to Terms of use & privacy policy</p>
             </div>
             <div className="login-forget">
-                <p className="login-toggle">Already have an account <span>Click here</span> </p>
+                {currentState==="Sign up" ?<p className="login-toggle">Already have an account <span onClick={()=>{setCurrentState("Login")}}>Login here</span> </p>:
+                <p className="login-toggle"> Create an account <span onClick={()=>{setCurrentState("Sign up")}}>Click here</span> </p>}
             </div>
         </form>
     </div>
