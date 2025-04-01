ad993465abdd719dd8d70b799f2532ad8a9dfda5
PavanSabani <pavansabani12@gmail.com>
2025-04-01 16:52:56 +0530

    chat data update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { createContext, useEffect, useState } from "react";
 import { auth, db } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
@@ -42,7 +42,26 @@ const AppContextProvider = (props) =>{
         }
     }
 
-    
+    useEffect(()=>{
+        if(userData){
+            const chatRef = doc(db,'chats',userData.id);
+            const unSub = onSnapshot(chatRef,async (res)=>{
+                const chatItems = res.data().chatData;
+                const tempdata =[];
+                for(const item of chatItems){
+                    const userRef = doc(db,'users',item.rId);
+                    const userSnap = await getDoc(userRef);
+                    const userData = userSnap.data();
+                    tempdata.push({...item,userData})
+                }
+                setChatData(tempdata.sort((a,b)=>{b.updatedAt - a.updatedAt}))
+            })
+            return ()=>{
+                unSub();
+            }
+
+        }
+    },[userData])
 
     const value = {
         userData,setUserData,
